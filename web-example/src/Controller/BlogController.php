c69e84e7928d7c89554562b905fd1d48188315d1
tmorales17 <tmorales17@jaumebalmes.net>
2019-05-27 17:24:30 +0200

    Basic pages ready

---
+++ web-example/src/Controller/BlogController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class BlogController extends AbstractController
+{
+	/**
+     * @Route("/", name="index")
+     */
+    public function renderIndexPage(Request $request): Response
+    {
+        return $this->render('personal/index.html.twig');
+    }
+	
+	/**
+     * @Route("/newspaper", name="news-page")
+     */
+    public function renderNewsPage(Request $request): Response
+    {
+        return $this->render('personal/news-page.html.twig');
+    }
+	
+	/**
+     * @Route("/message", name="message-page")
+     */
+    public function renderFeedbackPage(Request $request): Response
+    {
+        return $this->render('personal/feedback.html.twig');
+    }
+	
+	/**
+     * @Route("/about", name="about-page")
+     */
+    public function renderAboutPage(Request $request): Response
+    {
+        return $this->render('personal/about-page.html.twig');
+    }
+
+
+/**
+     * @Route("/contact", name="contact-page")
+     */
+    public function renderContactPage(Request $request): Response
+    {
+        return $this->render('personal/contact-page.html.twig');
+    }
+
+
+}
